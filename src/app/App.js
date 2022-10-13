6fa1b84355bc5d90cb6aab721d6b5904bfb11c95
기웅민 <kiwoongmin@giungmin-ui-MacBookPro.local>
2022-10-13 23:39:03 +0900

    feature: Header link & css

---
+++ src/app/App.js
@@ -0,0 +1,29 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from '../components/Home';
+import Header from '../components/Header';
+import IntroduceUs from '../components/IntroduceUs';
+import Product from '../components/Product';
+import Price from '../components/Price';
+import Support from '../components/Support';
+import Usage from '../components/Usage';
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/introduceUs" element={<IntroduceUs />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/price" element={<Price />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/usage" element={<Usage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
